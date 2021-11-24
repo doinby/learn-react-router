3adf1fc544ff4f22e8afb30bd40d1b9e7e3e07d8
Chinh Do <do.inby@gmail.com>
2021-11-24 20:43:36 +1100

    MUI Link now works with React Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import {Button} from '@mui/material';
+import React from 'react';
+// import {Link, Switch, Route} from 'react-router-dom';
+import {Link as RouterLink, Routes, Route} from 'react-router-dom';
+
+import Home from './components/Home';
+import About from './components/About';
+
 import {theme} from './theme';
 import {ThemeProvider} from '@emotion/react';
+import {Button, Link} from '@mui/material';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Button variant='contained'>Contained</Button>
-      <p>App Works!</p>
+      <Button component={RouterLink} to='/'>
+        Home
+      </Button>
+      <Button component={RouterLink} to='/about'>
+        About
+      </Button>
+
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/about' element={<About />} />
+      </Routes>
     </ThemeProvider>
   );
 }
-
-export default App;
