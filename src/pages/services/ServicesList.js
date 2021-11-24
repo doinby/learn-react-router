42e3f64974d56c5bec2d6bcd1cc0859420b555d2
Chinh <do.inby@gmail.com>
2021-11-25 10:59:10 +1100

    added service base files

---
+++ src/pages/services/ServicesList.js
@@ -0,0 +1,13 @@
+import React from "react"
+import serviceData from "./serviceData"
+
+function ServicesList() {
+    
+    return (
+        <div>
+            <h1>Services List Page</h1>
+        </div>
+    )
+}
+
+export default ServicesList
